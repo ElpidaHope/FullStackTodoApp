eef45d707bec4842731ab9597e2d712a07ee6023
ElpidaHope <paul.oladapo@yahoo.com.com>
2022-07-23 23:50:12 +0100

    change port

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middlewares/errorMiddleware')
 
 const app = express()
 connectDB()
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
